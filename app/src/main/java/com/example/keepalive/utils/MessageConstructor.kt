e5b604993bd034c2e242cfe076016f04aa35f0bd
madsunrise <rudnev.vanya@gmail.com>
2022-04-22 23:01:10 +0300

    Added OS Version to message

--- app/src/main/java/com/example/keepalive/utils/MessageConstructor.kt
+++ app/src/main/java/com/example/keepalive/utils/MessageConstructor.kt
@@ -1,10 +1,9 @@
 package com.example.keepalive.utils
 
 import android.os.Build
-import com.example.keepalive.BuildConfig
 
 object MessageConstructor {
     fun createMessage(text: String): String {
-        return "${Build.MODEL}: $text"
+        return "${Build.MODEL} (${Build.VERSION.RELEASE}): $text"
     }
 }
